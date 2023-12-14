2555be25522125aba9bd27d32f31643867401a47
heinrichefoulou <153495575+heinrichefoulou@users.noreply.github.com>
2023-12-14 15:24:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1646,7 +1646,7 @@ if page == pages[5]:
         """,
         unsafe_allow_html=True
     )
-  st.markdown('<p class="big-font">Résultat des profils à démarcher</p>', unsafe_allow_html=True)
+    st.markdown('<p class="big-font">Résultat des profils à démarcher</p>', unsafe_allow_html=True)
     st.write(summary)
     st.write(summary2)
     with st.container():
